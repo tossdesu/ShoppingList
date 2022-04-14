8a1e16a237e2f1fc20a11ca3e0319d1ac9760381
Toss <tossdesu@gmail.com>
2022-04-14 13:29:49 +0700

    add recyclerview adapter

--- app/src/main/java/com/tossdesu/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/tossdesu/shoppinglist/presentation/MainActivity.kt
@@ -5,27 +5,51 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.tossdesu.shoppinglist.R
+import com.tossdesu.shoppinglist.databinding.ActivityMainBinding
+import com.tossdesu.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainBinding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
+    lateinit var shopListAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
+        initAdapter()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("ActivityTest", it.toString())
+            shopListAdapter.shopList = it
         }
 
-        viewModel.shopList.value?.let {
-            viewModel.deleteShopItem(it[9])
-        }
+//        viewModel.shopList.value?.let {
+//            viewModel.deleteShopItem(it[9])
+//        }
+//
+//        viewModel.shopList.value?.let {
+//            viewModel.changeEnabledState(it[8])
+//        }
 
-        viewModel.shopList.value?.let {
-            viewModel.changeEnabledState(it[8])
-        }
+    }
 
+    private fun initAdapter() {
+        shopListAdapter = ShopListAdapter()
+        mainBinding.rvShopList.apply {
+            adapter = shopListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
+        shopListAdapter.setOnLongClickListener = {
+                viewModel.changeEnabledState(it)
+        }
     }
 }
