a0333ac888df0373d71cb3944efc80679197f10b
Toss <tossdesu@gmail.com>
2022-04-28 10:49:51 +0700

    24 Create shop item viewmodel with simple actions methods 25 Viewmodel _ Input errors handling, Getting shopitem in activity, Close edit/add screen event handling

---
+++ app/src/main/java/com/tossdesu/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,100 @@
+package com.tossdesu.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tossdesu.shoppinglist.data.ShopListRepositoryImpl
+import com.tossdesu.shoppinglist.domain.AddShopItemUseCase
+import com.tossdesu.shoppinglist.domain.EditShopItemUseCase
+import com.tossdesu.shoppinglist.domain.GetShopItemUseCase
+import com.tossdesu.shoppinglist.domain.ShopItem
+import java.lang.NumberFormatException
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val inputValid = validateInput(name, count)
+        if (inputValid) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val inputValid = validateInput(name, count)
+        if (inputValid) {
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: NumberFormatException) {
+            0
+        }
+    }
+
+    private fun validateInput(inputName: String, inputCount: Int): Boolean {
+        var result = true
+        if (inputName.isEmpty()) {
+            _errorInputName.value = true
+            result = false
+        }
+        if (inputCount <= 0) {
+            _errorInputCount.value = true
+            result = false
+        }
+        return result
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
+    }
+
+}
